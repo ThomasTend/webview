d43642bbbadd53c4d9b98c2dd3ad05fa297de20b
David Herrera <davidfherrerar@gmail.com>
2016-06-21 12:11:24 -0400

    Adding new services

---
+++ js/services2/patientService.js
@@ -0,0 +1,168 @@
+var myApp=angular.module('MUHCApp');
+
+myApp.service('Patient',['$q','$filter','LocalStorage','UserPreferences',function($q,$filter,LocalStorage,UserPreferences){
+    var ProfileImage='';
+    var FirstName='';
+    var LastName='';
+    var Alias='';
+    var TelNum='';
+    var Email='';
+    var PatientId='';
+    var UserSerNum='';
+    var NameFileSystem='';
+    var PathFileSystem='';
+    var CDVfilePath='';
+    return{
+        setUserFieldsOnline:function(patientFields){
+            var r=$q.defer();
+            patientFields=patientFields[0];
+            
+            UserPreferences.setEnableSMS(patientFields.EnableSMS);
+            UserPreferences.setLanguage(patientFields.Language);
+            console.log(UserPreferences.getLanguage());
+            UserPreferences.getFontSize();
+            if(typeof patientFields=='undefined') return;
+            FirstName=patientFields.FirstName;
+            LastName=patientFields.LastName;
+            Alias=patientFields.Alias;
+            TelNum=patientFields.TelNum;
+            Email=patientFields.Email;
+            PatientId=patientFields.PatientId;
+            UserSerNum=patientFields.PatientSerNum;
+            ProfileImage=(patientFields.ProfileImage)?'data:image/'+patientFields.DocumentType+';base64,'+patientFields.ProfileImage:'./img/patient.png';
+            var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+            if(app){
+                if(typeof patientFields.ProfileImage!=='undefined'||patientFields.ProfileImage=='')
+                {
+                  patientFields.ProfileImage='data:image/'+patientFields.DocumentType+';base64,'+patientFields.ProfileImage;
+                  patientFields.NameFileSystem='patient'+patientFields.PatientSerNum+"."+patientFields.DocumentType;
+                  var platform=$cordovaDevice.getPlatform();
+                  var targetPath='';
+                  if(platform==='Android'){
+                      targetPath = cordova.file.dataDirectory+'Patient/patient'+patientFields.PatientSerNum+"."+patientFields.DocumentType;
+                    patientFields.CDVfilePath="cdvfile://localhost/files/Patient/"+patientFields.NameFileSystem;
+                  }else if(platform==='iOS'){
+                    targetPath = cordova.file.documentsDirectory+ 'Patient/patient'+patientFields.PatientSerNum+"."+patientFields.DocumentType;
+                    patientFields.CDVfilePath="cdvfile://localhost/persistent/Patient/"+patientFields.NameFileSystem;
+
+                  }
+                  var url = patientFields.ProfileImage;
+                  delete patientFields.ProfileImage;
+                  var trustHosts = true
+                  var options = {};
+                  CDVfilePath=patientFields.CDVfilePath;
+                  NameFileSystem='patient'+patientFields.PatientSerNum+"."+patientFields.DocumentType;
+                  PathFileSystem=targetPath;
+                  patientFields.PathFileSystem=targetPath;
+                  LocalStorage.WriteToLocalStorage('Patient',[patientFields]);
+                  var promise=[FileManagerService.downloadFileIntoStorage(url, targetPath)];
+                  $q.all(promise).then(function()
+                  {
+                    r.resolve(patientFields);
+                  },function(error){
+            				console.log(error);
+            				r.resolve(patientFields);
+            			});
+                }else{
+                  ProfileImage='./img/patient.png';
+                  r.resolve(patientFields);
+                }
+              }else{
+                if(!patientFields.ProfileImage||typeof patientFields.ProfileImage=='undefined'||patientFields.ProfileImage=='')
+                {
+                  ProfileImage='./img/patient.png';
+
+                }
+                delete patientFields.ProfileImage;
+                LocalStorage.WriteToLocalStorage('Patient',[patientFields]);
+                r.resolve(patientFields);
+              }
+            console.log(UserPreferences.getLanguage());
+            return r.promise;
+        },
+        setUserFieldsOffline:function(patientFields)
+        {
+          var r=$q.defer();
+          patientFields=patientFields[0];
+          UserPreferences.setEnableSMS(patientFields.EnableSMS);
+          UserPreferences.setLanguage(patientFields.Language);
+          UserPreferences.getFontSize();
+          FirstName=patientFields.FirstName;
+          LastName=patientFields.LastName;
+          Alias=patientFields.Alias;
+          TelNum=patientFields.TelNum;
+          Email=patientFields.Email;
+          PatientId=patientFields.PatientId;
+          UserSerNum=patientFields.PatientSerNum;
+          ProfileImage= (patientFields.ProfileImage)?patientFields.ProfileImage:'./img/patient.png';
+          Alias=patientFields.Alias;
+          if(patientFields.PathFileSystem)
+          {
+            ProfileImage=patientFields.CDVfilePath;
+            console.log(ProfileImage);
+            /*var promise=[FileManagerService.getFileUrl(patientFields.PathFileSystem)];
+            $q.all(promise).then(function(result){
+              console.log(result);
+              patientFields.ProfileImage=result[0];
+              ProfileImage=result[0];
+              console.log(ProfileImage);
+              r.resolve(patientFields);
+            },function(error){
+              console.log(error);
+              r.resolve(patientFields);
+            });*/
+            r.resolve(true);
+          }else{
+            ProfileImage='./img/patient.png';
+            r.resolve(true);
+          }
+          return r.promise;
+        },
+        setFirstName:function(name){
+            FirstName=name;
+        },
+        setLastName:function(name){
+            LastName=name;
+        },
+        setAlias:function(name){
+            Alias=name;
+        },
+        setTelNum:function(telephone){
+            TelNum=telephone;
+        },
+        setEmail:function(email){
+            Email=email;
+        },
+        getPatientId:function()
+        {
+          return PatientId;
+        },
+        getFirstName:function(){
+            return FirstName;
+        },
+        getLastName:function(){
+            return LastName;
+        },
+        getAlias:function(){
+            return Alias;
+        },
+        getTelNum:function(){
+            return TelNum;
+        },
+        getEmail:function(){
+            return Email;
+        },
+        getUserSerNum:function(){
+            return UserSerNum;
+        },
+        setProfileImage:function(img){
+            ProfileImage='data:image/png;base64,'+img;
+        },
+        getProfileImage:function(){
+            return ProfileImage;
+        },
+        getStatus:function(){
+            return Status;
+        }
+    };
+}]);
---
+++ js/services2/requestToServerService.js
@@ -0,0 +1,112 @@
+var myApp=angular.module('MUHCApp');
+/**
+*
+*
+*
+**/
+myApp.service('RequestToServer',function(UserAuthorizationInfo, EncryptionService, FirebaseService, $http,$q){
+    var identifier='';
+    var Ref=new Firebase(FirebaseService.getFirebaseUrl()+'requests');
+     var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+      if(app){
+          identifier=device.uuid;
+      }else{
+          identifier='browser';
+      }
+    var lastUpdateTimestamp={};
+    function initTimestampsFromLocalStorage()
+    {
+      lastUpdateTimestamp=JSON.parse(window.localStorage.getItem(UserAuthorizationInfo.UserName+'/Timestamps'));
+    }
+    function initTimestamps(time)
+    {
+
+      lastUpdateTimestamp={
+        'All':time,
+        'Appointments':time,
+        'Messages':time,
+        'Documents':time,
+        'Tasks':time,
+        'Doctors':time,
+        'LabTests':time,
+        'Patient':time,
+        'Notifications':time,
+        'EducationalMaterial':time
+      };
+      window.localStorage.setItem(UserAuthorizationInfo.UserName+'/Timestamps',JSON.stringify(lastUpdateTimestamp));
+    }
+    function obtainTimestamp(content)
+    {
+      if(typeof content=='undefined')
+      {
+        return lastUpdateTimestamp.All;
+      }else if(angular.isArray(content))
+      {
+        var min=Infinity;
+        for (var i = 0; i < content.length; i++) {
+          if(min>lastUpdateTimestamp[content[i]])
+          {
+            min=lastUpdateTimestamp[content[i]];
+          }
+        }
+        return min;
+      }else{
+        return lastUpdateTimestamp[content];
+      }
+    }
+    return{
+        sendRequest:function(typeOfRequest,content){
+          //Deciding whether is an app or a website
+          console.log('I am in there');
+          var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+          var userID=UserAuthorizationInfo.UserName;
+          var token=UserAuthorizationInfo.Token;
+          var time=(new Date()).getTime();
+          var encryptedRequestType=EncryptionService.encryptData(typeOfRequest);
+          var timestamp=null;
+          console.log(typeOfRequest);
+          if(typeOfRequest=='Login'||typeOfRequest=='Resume')
+          {
+              initTimestamps(time);
+          }else if(typeOfRequest=='Refresh')
+          {
+
+            if(!lastUpdateTimestamp.hasOwnProperty('All')) initTimestampsFromLocalStorage();
+            timestamp=obtainTimestamp(content);
+
+            console.log(lastUpdateTimestamp);
+          }
+          content= EncryptionService.encryptData(content);
+          if(app){
+            //If online send request as normal
+            Ref.push({ 'Request' : encryptedRequestType,'DeviceId':identifier, 'Token':token, 'UserID': userID, 'Parameters':content,'Timestamp':timestamp });
+          }else{
+            //If its not an app just try sending the request
+            Ref.push({ 'Request' : encryptedRequestType,'DeviceId':identifier,'Token':token,  'UserID': userID, 'Parameters':content,'Timestamp':timestamp});
+          }
+
+        },
+        updateTimestamps:function(content,time)
+        {
+          if(content=='All')
+          {
+            initTimestamps(time);
+          }else if(angular.isArray(content))
+          {
+            for (var i = 0; i < content.length; i++) {
+              lastUpdateTimestamp[content[i]]=time;
+            }
+          }else{
+            lastUpdateTimestamp[content]=time;
+          }
+          window.localStorage.setItem(UserAuthorizationInfo.UserName+'/Timestamps',JSON.stringify(lastUpdateTimestamp));
+        },
+        getIdentifier:function()
+        {
+          return identifier;
+        }
+    };
+
+
+
+});
